bb2ec7ee920a4a14717c45b9746fb9c4d76193bb
Kohányi Róbert <kohanyi.robert@gmail.com>
2017-05-29 11:11:15 +0000

    Add README and script.cmd

---
+++ script.cmd
@@ -0,0 +1,3 @@
+mklink %USERPROFILE%\.gitconfig %USERPROFILE%\Work\.gitconfig
+mklink %USERPROFILE%\.minttyrc %USERPROFILE%\Work\.minttyrc
+mklink /D %USERPROFILE%\.mintty %USERPROFILE%\Work\.mintty
