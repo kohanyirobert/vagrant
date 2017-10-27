f28b1492526e8163e276a7d453b16f017c533999
Kohányi Róbert <kohanyi.robert@gmail.com>
2017-10-27 14:42:07 +0000

    Symlink .aws config folder to %USERPROFILE%

--- script.ps1
+++ script.ps1
@@ -4,3 +4,4 @@ Get-ChildItem .ssh\id_rsa* | ForEach-Object {
 New-Item -ItemType SymbolicLink -Value .gitconfig -Path (Join-Path $env:USERPROFILE ".gitconfig") -Force
 New-Item -ItemType SymbolicLink -Value .minttyrc -Path (Join-Path $env:USERPROFILE ".minttyrc") -Force
 New-Item -ItemType SymbolicLink -Value .mintty -Path (Join-Path $env:USERPROFILE ".mintty") -Force
+New-Item -ItemType SymbolicLink -Value .aws -Path (Join-Path $env:USERPROFILE ".aws") -Force
