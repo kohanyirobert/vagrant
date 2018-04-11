dcb94dd98703f305336a2e1fe010a4d29cd0d697
Kohányi Róbert <kohanyi.robert@gmail.com>
2018-04-11 07:48:41 +0000

    Update script.ps1 to generate a working .bash_profile for msys
    The previous ssh-agent sock handling didn't work so I've redone it using
    this description:
    https://www.electricmonk.nl/log/2012/04/24/re-use-existing-ssh-agent-cygwin-et-al/.
    The key was starting ssh-agent using the -a flag with which one can
    supply a socket file for it to use. Other approaches didn't seem to
    work.

--- script.ps1
+++ script.ps1
@@ -6,18 +6,16 @@ New-Item -ItemType SymbolicLink -Value .minttyrc -Path (Join-Path $env:USERPROFI
 New-Item -ItemType SymbolicLink -Value .mintty -Path (Join-Path $env:USERPROFILE ".mintty") -Force
 New-Item -ItemType SymbolicLink -Value .aws -Path (Join-Path $env:USERPROFILE ".aws") -Force
 [IO.File]::WriteAllLines((Join-Path $env:USERPROFILE ".bash_profile"), @'
-MY_SSH_AUTH_SOCK=~/.ssh/ssh_auth_sock
+export SSH_AUTH_SOCK=~/.ssh/ssh_auth_sock
 ssh-add &> /dev/null
 if [ $? -ne 0 ]
 then
-    rm $MY_SSH_AUTH_SOCK
+    rm $SSH_AUTH_SOCK
 fi
-if [ ! -S $MY_SSH_AUTH_SOCK ]
+if [ ! -S $SSH_AUTH_SOCK ]
 then
-  eval $(ssh-agent -s)
-  ln -fsv $SSH_AUTH_SOCK $MY_SSH_AUTH_SOCK
+  eval $(ssh-agent -s -a $SSH_AUTH_SOCK)
 fi
-export SSH_AUTH_SOCK=$MY_SSH_AUTH_SOCK
 ssh-add -l | grep "The agent has no identities" && ssh-add $(find $HOME/.ssh/ -name id_rsa* -not -name id_rsa*.pub)
 export HISTCONTROL=ignoreboth:erasedups
 '@) 
