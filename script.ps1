3dd420cf2ee7c0e39606244f27b3ac39b29f234c
Kohányi Róbert <kohanyi.robert@gmail.com>
2018-04-17 08:14:31 +0200

    Link .vimrc to %USERPROFILE% for to use inside msys environment.
    Clone Vundle repo, becase the vim config relies on it.

--- script.ps1
+++ script.ps1
@@ -1,3 +1,4 @@
+git clone git@github.com:VundleVim/Vundle.vim.git (Join-Path $env:USERPROFILE ".vim\bundle\Vundle.vim")
 Get-ChildItem .ssh\id_rsa* | ForEach-Object {
   New-Item -ItemType SymbolicLink -Value $_.FullName -Path (Join-Path $env:USERPROFILE ".ssh" | Join-Path -ChildPath $_.Name) -Force
 }
@@ -5,6 +6,7 @@ New-Item -ItemType SymbolicLink -Value .gitconfig -Path (Join-Path $env:USERPROF
 New-Item -ItemType SymbolicLink -Value .minttyrc -Path (Join-Path $env:USERPROFILE ".minttyrc") -Force
 New-Item -ItemType SymbolicLink -Value .mintty -Path (Join-Path $env:USERPROFILE ".mintty") -Force
 New-Item -ItemType SymbolicLink -Value .aws -Path (Join-Path $env:USERPROFILE ".aws") -Force
+New-Item -ItemType SymbolicLink -Value .vimrc -Path (Join-Path $env:USERPROFILE ".vimrc") -Force
 [IO.File]::WriteAllLines((Join-Path $env:USERPROFILE ".bash_profile"), @'
 export SSH_AUTH_SOCK=~/.ssh/ssh_auth_sock
 ssh-add &> /dev/null
