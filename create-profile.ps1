061835390578bae6ed28e066707e6055c6713739
Kohányi Róbert <kohanyi.robert@gmail.com>
2020-08-21 10:51:09 +0200

    Functions to enable/disable Docker Toolbox

--- create-profile.ps1
+++ create-profile.ps1
@@ -4,5 +4,14 @@ Out-File -FilePath $profile -InputObject @'
 Import-Module posh-sshell
 Import-Module posh-git
 Start-SshAgent
-docker-machine env | Invoke-Expression
+function Enable-Docker-Toolbox {
+  docker-machine env | Invoke-Expression
+}
+function Disable-Docker-Toolbox {
+  docker-machine env | Invoke-Expression
+  del -ErrorAction Ignore env:\DOCKER_TLS_VERIFY
+  del -ErrorAction Ignore env:\DOCKER_CERT_PATH
+  del -ErrorAction Ignore env:\DOCKER_HOST
+  del -ErrorAction Ignore env:\DOCKER_TOOLBOX_INSTALL_PATH
+}
 '@
